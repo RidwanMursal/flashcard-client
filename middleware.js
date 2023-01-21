e37e95c6a4f560ff58d4d6e92e28afc5d8db0a28
ridwanmursal <ridwan09@hotmail.com>
2023-01-21 17:08:47 -0500

    edited middleware

--- middleware.js
+++ middleware.js
@@ -11,7 +11,8 @@ export const middleware = (req) => {
   // error occurs here if you come from authorized to unauthorized
   // ie if you delete the && !cookie part. This has to do with context
   /// come back to it later
-  if (url.includes("/dashboard")) {
-    return NextResponse.redirect("https://flashcard-client.vercel.app/");
-  }
+  //   if (url.includes("/dashboard")) {
+  //     return NextResponse.redirect("https://flashcard-client.vercel.app/");
+  //   }
+  return;
 };
