fc82e05d19808a147e54dbae4d28e281e114f0d7
ridwanmursal <ridwan09@hotmail.com>
2023-01-21 16:52:11 -0500

    edited middleware

--- middleware.js
+++ middleware.js
@@ -3,7 +3,8 @@ import { NextResponse } from "next/server";
 export const middleware = (req) => {
   const url = req.url;
   const cookie = req.cookies.get("refreshToken");
-  console.log("THIS IS THE COOKIE");
+  console.log("THIS IS THE URL", cookie);
+  console.log("THIS IS THE COOKIE", cookie);
   //console.log(cookie)
   //console.log(`in middleware, the url is ${url}`)
 
